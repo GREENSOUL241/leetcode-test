9139a41869b2b6c30e6c4393d61e14f693169712
Aka Patil <pixellpenguiin@gmail.com>
2026-08-31 20:34:39 +0530

    Fix increment of left pointer in substring logic

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,7 +7,7 @@ class Solution:
         for right in range(len(s)):
             while s[right] in char_set:
                 char_set.remove(s[left])
-                left += 1
+                left =+ 1
 
             char_set.add(s[right])
             max_length = max(max_length, right - left + 1)
